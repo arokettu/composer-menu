1767a45c72a04e3e9576114f8a90677ba149c59a
Anton Smirnov <sandfox@sandfox.me>
2022-06-08 01:51:35 +0300

    Basically the whole plugin

---
+++ ComposerMenuPlugin.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Arokettu\ComposerMenu;
+
+use Arokettu\ConsoleMenu\ConsoleMenuCommand;
+use Composer\Composer;
+use Composer\IO\IOInterface;
+use Composer\Plugin\Capability\CommandProvider;
+use Composer\Plugin\Capable;
+use Composer\Plugin\PluginInterface;
+
+class ComposerMenuPlugin implements PluginInterface, Capable, CommandProvider
+{
+    public function getCapabilities(): array
+    {
+        return [
+            CommandProvider::class => self::class,
+        ];
+    }
+
+    public function getCommands(): array
+    {
+        return [
+            new ConsoleMenuCommand(),
+        ];
+    }
+
+    public function activate(Composer $composer, IOInterface $io): void
+    {
+    }
+
+    public function deactivate(Composer $composer, IOInterface $io): void
+    {
+    }
+
+    public function uninstall(Composer $composer, IOInterface $io): void
+    {
+    }
+}
